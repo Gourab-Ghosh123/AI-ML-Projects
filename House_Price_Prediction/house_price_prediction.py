c7d1ab38d1ffd941b02748adb9b1fd93c20dccac
Gourab-Ghosh123 <ggourab2004@gmail.com>
2026-06-14 01:45:07 +0530

    Error Evaluation

--- House_Price_Prediction/house_price_prediction.py
+++ House_Price_Prediction/house_price_prediction.py
@@ -1,19 +1,55 @@
 
 import pandas as pd
+
 from sklearn.linear_model import LinearRegression
 
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import(
+    mean_absolute_error ,
+    mean_squared_error,
+    r2_score
+    )
+
 data = {
-    "size" : [1000 , 1200 , 1500 , 1800 , 2000],
-    "price" : [10 , 12 , 15 , 18 ,20]
+    "size":[1000,1200,1500,1800,2000,2200,2500,2800],
+    "price":[10,12,15,18,20,22,25,28]
 }
 
 df = pd.DataFrame(data)
 
 X = df[["size"]]
+
 y = df["price"]
 
-model = LinearRegression()
-model.fit(X , y)
+X_train , X_test , y_train , y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
 
-prediction = model.predict([[2500]])
-print(prediction)
+model = LinearRegression()
+
+model.fit(X_train , y_train)
+
+predictions = model.predict(X_test)
+
+mae = mean_absolute_error(
+    y_test,
+    predictions
+)
+
+mse = mean_squared_error(
+    y_test,
+    predictions
+)
+
+r2 = r2_score(
+    y_test,
+    predictions
+)
+
+print("MAE :" , mae)
+print("MSE : " , mse)
+print("r2 Score : " , r2)
